efcf652795a77462aa171d63b13835b1c2b2d00e
debjitdey1 <debjitdey559@gmail.com>
2024-03-25 12:45:35 +0530

    GitHub-contribution-graph

---
+++ app.js
@@ -0,0 +1,16 @@
+const cont = document.querySelector(".container");
+console.log(cont);
+const colors = ["#196127", "#219A3A", "#EAEDF0", "#C6E48B", "7BC96F"];
+let square_count = 365;
+for (let i = 0; i < 365; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.style.backgroundColor = randomecolor(colors);
+  cont.appendChild(square);
+}
+
+function randomecolor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+console.log("randome Color", randomecolor(colors));
